a6d59075e7b2a7f48d1b1023d10639414337b2f1
Lex [VisiTech] <Lex.Yudin@VisiTech.ru>
2016-08-24 16:57:24 +0300

    [BUG 1573] DefaultSettings clone bug

--- html/js/game.js
+++ html/js/game.js
@@ -1,14 +1,14 @@
 (function(){
 
-	var canvas = document.getElementById("THE_SNAKE"),
-		ctx = canvas.getContext("2d");
-		size = 50,
-		pixelsize = 15,
-		props = [],
-		scores = 0,
-		isGameRun = false,
-		isPause = false,
-		isGameOver = false;
+	var canvas = document.getElementById("THE_SNAKE");
+	var ctx = canvas.getContext("2d");
+	var size = 50;
+	var pixelsize = 15;
+	var props = [];
+	var scores = 0;
+	var isGameRun = false;
+	var isPause = false;
+	var isGameOver = false;
 
 	var defaultSettings = {
 		ui: {
@@ -48,7 +48,7 @@
     	return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
 	}
 
-	var prop = function(index,x,y, settings){
+	function Prop(index,x,y, settings){
 		var _scores = settings.scores,
 			_colors = settings.colors,
 			_calories = settings.calories,
@@ -62,7 +62,6 @@
 		this.framesLives = _framesLives[index];
 		this.isKilling = _isKillings[index];
 	};
-
 
 	var game = new (function(){
 		var _ctx = ctx,
@@ -90,41 +89,38 @@
 		this.updateResults = updateResults;
 		this.message = message;
 
-
 		function initUI(UISettings){
 			defaultSettings.ui = UISettings ? Object.assign(defaultSettings.ui, UISettings) : defaultSettings.ui;
+
 			if(_settings){
 				_settings.ui = defaultSettings.ui;
-			}else
-			{
+			}else{
 				_settings = {
 					ui: defaultSettings.ui
 				};
 			}
 		}
+
 		function __init__(settings){
 
-			//console.log(defaultSettings);
+			_settings = {};
 
-			defaultSettings.game = settings.game ? Object.assign(defaultSettings.game, settings.game) : defaultSettings.game;
-			defaultSettings.props = settings.props ? Object.assign(defaultSettings.props, settings.props) : defaultSettings.props;
-			defaultSettings.snake = settings.snake ? Object.assign(defaultSettings.snake, settings.snake) : defaultSettings.snake;
-			defaultSettings.ui = settings.ui ? Object.assign(defaultSettings.ui, settings.ui) : defaultSettings.ui;
-			_settings = defaultSettings;
+			_settings.game = settings.game ? Object.assign(defaultSettings.game, settings.game) : defaultSettings.game;
+			_settings.props = settings.props ? Object.assign(defaultSettings.props, settings.props) : defaultSettings.props;
+			_settings.snake = settings.snake ? Object.assign(defaultSettings.snake, settings.snake) : defaultSettings.snake;
+			_settings.ui = settings.ui ? Object.assign(defaultSettings.ui, settings.ui) : defaultSettings.ui;
 
 			if(settings.name){
 				document.body.className = settings.name;
 				_gameModeName = settings.name;
 			}
 
-			//this.updateSa
 			updateResults();
-			//console.log(defaultSettings);
 
 			snake.__init__(_settings.snake, this);
 
 			scores = 0;
-			
+
     		size = _settings.game.size;
     		pixelsize = _settings.game.pixelsize;
     		_fps = _settings.game.fps;
@@ -134,54 +130,27 @@
 
 			canvas.width = size*pixelsize;
 			canvas.height = size*pixelsize;
-		};
+		}
 
 		function initWithMode(mode){
-			var difficulties = [
-				{
-					name: "EasyMode",
-
-					props:{
-						chanceOfBomb: 0.1,
-					},
-					game:{
-						lives: 3,
-						fps: 10
-					},
-					snake:{
-						additionalSpeed: 0.08
-					}
-				},
-				{
+			var difficulties = [{
+					name:	"EasyMode",
+					props:	{ chanceOfBomb: 0.1 },
+					game:	{ lives: 3, fps: 10 },
+					snake:	{ additionalSpeed: 0.08 }
+				},{
 					name: "NormalMode",
-
-					props:{
-						chanceOfBomb: 0.25,
-					},
-					game:{
-						lives: 2
-					},
-					snake:{
-						additionalSpeed: 0.4
-					}
-				},
-				{
+					props:	{ chanceOfBomb: 0.25 },
+					game:	{ lives: 2 },
+					snake:	{ additionalSpeed: 0.4 }
+				},{
 					name: "HardCoreMode",
-
-					props:{
-						chanceOfBomb: 0.5,
-					},
-					game:{
-						lives: 1,
-						fps: 20
-					},
-					snake:{
-						additionalSpeed: 0.7
-					}
+					props:	{ chanceOfBomb: 0.5 },
+					game:	{ lives: 1, fps: 20 },
+					snake:	{ additionalSpeed: 0.7 }
 				}
 			];
 
-			//restartGame();
 			this.__init__(typeof mode !== "undefined" ? difficulties[mode] : difficulties[prompt("0 - Easy, 1 - Normal, 2 - Hardcore")]);
 			this.run();
 			this.pause();
@@ -189,19 +158,23 @@
 
 		function sendResults(result){
 			var xhr = new XMLHttpRequest();
+
 			xhr.open("POST", "/Results", true);
 			xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
 			xhr.send(JSON.stringify({
 				usrID: _userID,
 				name: result[0],
 				modeName: result[2],
 				score: result[3]
 			}));
+
 			updateResults();
-		};
+		}
 
 		function updateResults(){
 			var xhttp = new XMLHttpRequest();
+
 			xhttp.onreadystatechange = function() {
 				if (xhttp.readyState == 4 && xhttp.status == 200) {
 			    	var htmlTable = '<tr align="left" class="header"><th>№</th><th>User Name</th><th>Game Mode</th><th>Scores</th></tr>';
@@ -214,19 +187,21 @@
 			    	document.querySelector(_settings.ui.results_table).innerHTML = htmlTable;
 				}
 			};
+
 			xhttp.open("GET", "/Results?usr="+_userID, true);
   			xhttp.send();
-		};
+		}
 
 		function onEat(){
-			//console.log("EAT");
 			if(_fps < _settings.snake.max_speed)
 				_fps += _settings.snake.additionalSpeed;
+
 			_fpsInterval = 1000 / _fps;
-		};
+		}
 
 		function dead(){
 			_lives--;
+
 			if(_lives <= 0){
 				isGameRun = false;
 				isGameOver = true;
@@ -236,58 +211,56 @@
 					_userName = prompt("Enter Your Name");
 					localStorage.userName = _userName;
 				}
+
 				localStorage.record = localStorage.record ? (localStorage.record <= scores ? scores : localStorage.record) : scores;
-				//console.log();
 				sendResults([_userName, _userID, _gameModeName, scores]);
-				//alert("You're failed!!!");
-				//console.log("FAIL");
 			}else{
 				snake.__init__(_settings.snake, this);
 			}
-		};
+		}
 
 		function run(){
-
 			_fpsInterval = 1000 / _fps;
     		_then = Date.now();
     		_startTime = _then;
 
-    		//_settings = settings ? settings : _settings;
-
     		isGameRun = true;
 			window.game.loop();
-		};
+		}
 
 		function pause(){
 			if(isGameRun){
 				isPause = true;
 				isGameRun = false;
 				message("Game Paused (Press P)");
-			}
-			else{
+			}else{
 				message("");
 				isPause = false;
 				isGameRun = true;
 			}
-
-			//document.querySelector(_settings.ui.main_canvas).className = isPause ? "paused" : "run";
 		}
+
 		function restartGame(){
 			snake.__init__(_settings.snake, this);
+
 			scores = 0;
 			_FramesFromLastProp = 0;
 			_fps =_settings.game.fps;
 			_fpsInterval = 1000 / _fps;
 			_lives = _settings.game.lives;
+
 			props = [];
+
 			isGameRun = true;
 			isGameOver = false;
 			isPause = false;
+
 			message("");
-		};
+		}
 
 		function loop(){
 			requestAnimationFrame(loop);
+
 			_now = Date.now();
     		_elapsed = _now - _then;
 
@@ -296,18 +269,18 @@
 
 				updateAndDrow();
 			}
-		};
+		}
+
 		function updateAndDrow()
 		{
 			_ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-			/////////////////
-			//Props generation
-			////////////////
-			
-			//console.log(_fps);
-
 			if(isGameRun){
+
+				/////////////////
+				//Props generation
+				////////////////
+
 				if(_FramesFromLastProp >= _FramesPerProp && props.length < _maxPropsCount){
 
 					var newPropX = Math.floor(Math.random()*size),
@@ -315,11 +288,11 @@
 					do{
 						newPropY = Math.floor(Math.random()*size);
 						newPropY = Math.floor(Math.random()*size);
-					}while(snake.blocks[newPropX][newPropY].enabled);
+					}
+					while(snake.blocks[newPropX][newPropY].enabled);
 
 					var newPropType = Math.random() <= _settings.props.chanceOfBomb ? (Math.random() >= 0.7 ? 3 : 1) : (Math.random() >= 0.5 ? 2 : 0);
-
-					var newProp = new prop(newPropType, newPropX, newPropY, _settings.props);
+					var newProp = new Prop(newPropType, newPropX, newPropY, _settings.props);
 					props.push(newProp);
 
 					_FramesFromLastProp = 0;
@@ -331,44 +304,33 @@
 					props[index].framesLives--;
 					return props[index].framesLives > 0;
 				});
+			
+				/////////////////
+				//Snake updates
+				////////////////
 
-			
-			
-			
+				snake.update();
+			}
+
 			/////////////////
-			//Snake updates
+			//Props Drow
 			////////////////
-			snake.update();
-
-			};
 
 			props.forEach(function(prop){
 				_ctx.fillStyle = prop.color;
 				_ctx.fillRect(prop.point.x*pixelsize,prop.point.y*pixelsize,pixelsize,pixelsize);
 			});
 
-			snake.drow(_ctx);
-
 			/////////////////
-			//Score text update
+			//Snake Drow
 			////////////////
-			
 
-			// _ctx.fillStyle
-			// _ctx.font = "48px serif";
-			// _ctx.fillText(scores, size*pixelsize-100, 50);
-
-			// //ctx.font = "48px serif";
-			// ctx.fillText(_lives, 0, 50);
-
-
-			
-
-
+			snake.drow(_ctx);
 
 			/////////////////
 			//Drow grid
 			////////////////
+
 			ctx.strokeStyle="#f1f1f1";
 
 			for (var i = 1; i < size; i++) {
@@ -384,10 +346,13 @@
 				ctx.stroke();
 			}
 
+			/////////////////
+			//Labels update
+			////////////////
+
 			updateStatus();
 		}
 
-		//this.__init__();
 		function updateStatus(){
 
 			var lives_label = document.querySelector(_settings.ui.lives_label);
@@ -409,27 +374,27 @@
 
 		function message(msg){
 			document.querySelector(_settings ? _settings.ui.status_label : defaultSettings.ui.status_label).textContent = msg;
-		};
+		}
 
 	})();
 
 
 	var snake = new (function(){
-		var _snakeBlocks = [],
-			_direction = "up",
-			_directionToSet = null,
-			_anchorPoint = {x: 0, y: 0},
-			_backAnchorPoint = {x: 0, y: 0},
-			_snakeLength = 1,
-			_needsToAddBlockCount = 0;
-			_isAlive = true,
+		var _snakeBlocks = [];
+		var _direction = "up";
+		var _directionToSet = null;
+		var _anchorPoint = {x: 0, y: 0};
+		var _backAnchorPoint = {x: 0, y: 0};
+		var _snakeLength = 1;
+		var _needsToAddBlockCount = 0;
+		var _isAlive = true;
 
-			_snakeColor = {
+		var _snakeColor = {
 				alive: "#0000FF",
 				dead: "#FF0000"
-			},
+			};
 
-			_gameObj = null;
+		var _gameObj = null;
 
 		this.__init__ = __init__;
 		this.setDirection = setDirection;
@@ -438,8 +403,7 @@
 		this.update = update;
 		this.drow = drow;
 
-
-		var snakeBlock = function(enabled, direction){
+		function SnakeBlock(enabled, direction){
 			this.enabled = enabled;
 			this.direction = direction;
 
@@ -453,7 +417,7 @@
 			this.toString = function(){
 				return this.enabled ? "1" : "0";
 			};
-		};
+		}
 
 		function __init__(settings, game){
 			_gameObj = game;
@@ -464,31 +428,28 @@
 			for (var x = 0; x < size; x++) {
 				_snakeBlocks[x] = [];
 				for (var y = 0; y < size; y++) {
-					_snakeBlocks[x][y] = new snakeBlock(false, "none");
+					_snakeBlocks[x][y] = new SnakeBlock(false, "none");
 				}
 			}
 
-			var rndX = 	Math.floor(Math.random()*(size/2)+size/4),
-				rndY = 	Math.floor(Math.random()*(size/2)+size/4);
+			var rndX = 	Math.floor(Math.random()*(size/2)+size/4);
+			var rndY = 	Math.floor(Math.random()*(size/2)+size/4);
 
 			_snakeLength = 1;
 			_direction = "up";
 			_directionToSet = null;
 			_isAlive = true;
 			_needsToAddBlockCount = 0;
-			//create head
+
 			_anchorPoint.x = rndX;
 			_anchorPoint.y = rndY;
 			_snakeBlocks[_anchorPoint.x][_anchorPoint.y].create(_direction); 
 
 			_backAnchorPoint.x = _anchorPoint.x;
 			_backAnchorPoint.y = _anchorPoint.y;
-
-
-		};
+		}
 
 		function setDirection(val){
-			
 			if(!isGameRun)
 				return;
 			if(_direction == "right" && val == "left")
@@ -501,17 +462,16 @@
 				return;
 
 			_directionToSet = val;
-			
-		};
+		}
 
 		function addBlocks(count){
 			_needsToAddBlockCount += count;
-		};
+		}
 
 		 function kill(){
 			_isAlive = false;
 			_gameObj.dead();
-		};
+		}
 
 		function update(){
 
@@ -542,11 +502,13 @@
 
 			//////////////////////
 			// DUMB WAYS TO DIE
-			//
+			/////////////////////
+
 			if( _snakeLength + _needsToAddBlockCount <= 0 || _anchorPoint.x < 0 || _anchorPoint.y < 0 || _anchorPoint.x >= size || _anchorPoint.y >= size || _snakeBlocks[_anchorPoint.x][_anchorPoint.y].enabled){
 				this.kill();
 				return;
 			}
+
 			/////////////////////
 
 			props = props.filter(function(prop){
@@ -567,7 +529,6 @@
 				return;
 
 			_snakeBlocks[_anchorPoint.x][_anchorPoint.y].create(_direction);
-
 
 			if(_needsToAddBlockCount < 1){
 				var firstLoop = true;
@@ -592,8 +553,8 @@
 					if(!firstLoop){
 						_needsToAddBlockCount+= _needsToAddBlockCount < 0 ? 1 : 0;
 						_snakeLength--;
-					}else
-					{
+					}else{
+
 						firstLoop = false;
 					}
 				}
@@ -619,15 +580,11 @@
 			}
 		};
 
-		//this.__init__();
 		this.blocks = _snakeBlocks;
 	})();
 
 
-
 	window.game = game;
-
-	//game.initWithMode();
 	game.initUI();
 	game.updateResults();
 	game.message("Select mode by key (Z, X, C)");
@@ -635,47 +592,54 @@
 	setInterval(function(){
 		game.updateResults();
 	}, 3000);
-	//game.pause();
 
 	window.addEventListener("keydown", function(e){
-		console.log(e.keyCode);
 		switch(e.keyCode){
 			case 37:
 			case 65:
-			snake.setDirection("left");
-			break;
+				snake.setDirection("left");
+				e.preventDefault();
+				break;
 			case 38:
 			case 87:
-			snake.setDirection("up");
-			break;
+				snake.setDirection("up");
+				e.preventDefault();
+				break;
 			case 39:
 			case 68:
-			snake.setDirection("right");
-			break;
+				snake.setDirection("right");
+				e.preventDefault();
+				break;
 			case 40:
 			case 83:
-			snake.setDirection("down");
-			break;
+				snake.setDirection("down");
+				e.preventDefault();
+				break;
 			case 80:
-			game.pause();
-			break;
+				game.pause();
+				e.preventDefault();
+				break;
 			case 77:
-			game.initWithMode();
-			break;
+				game.initWithMode();
+				e.preventDefault();
+				break;
 			case 90:
-			game.initWithMode(0);
-			break;
+				game.initWithMode(0);
+				e.preventDefault();
+				break;
 			case 88:
-			game.initWithMode(1);
-			break;
+				game.initWithMode(1);
+				e.preventDefault();
+				break;
 			case 67:
-			game.initWithMode(2);
-			break;
+				game.initWithMode(2);
+				e.preventDefault();
+				break;
 			case 82:
-			game.restartGame();
-			break;
+				game.restartGame();
+				e.preventDefault();
+				break;
 		}
-		e.preventDefault();
 	});
 
 })();
