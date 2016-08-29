5f9d2d356f9c2372728584e1b4b0b8ba2f780ee1
Lex [VisiTech] <Lex.Yudin@VisiTech.ru>
2016-08-29 12:38:13 +0300

    [TASK 1593] Optimize ES5 Code

--- html/js/game_keyboardEvents.js
+++ html/js/game_keyboardEvents.js
@@ -1,21 +1,49 @@
 (function(){
 
-var keyEvents = {};
-
-keyEvents[37] = function(){ window.gameData.snake.setDirection("left"); };
-keyEvents[65] = keyEvents[37];
-keyEvents[38] = function(){ window.gameData.snake.setDirection("up"); };
-keyEvents[87] = keyEvents[38];
-keyEvents[39] = function(){ window.gameData.snake.setDirection("right"); };
-keyEvents[68] = keyEvents[39];
-keyEvents[40] = function(){ window.gameData.snake.setDirection("down"); };
-keyEvents[83] = keyEvents[40];
-keyEvents[80] = function(){ window.game.pause(); };
-keyEvents[77] = function(){ window.game.initWithMode(); };
-keyEvents[90] = function(){ window.game.initWithMode(0); };
-keyEvents[88] = function(){ window.game.initWithMode(1); };
-keyEvents[67] = function(){ window.game.initWithMode(2); };
-keyEvents[82] = function(){ window.game.restartGame(); };
+var keyEvents = {
+	37: function(){
+		window.gameData.snake.setDirection("left");
+	},
+	65: function(){
+		window.gameData.snake.setDirection("left");
+	},
+	38: function(){
+		window.gameData.snake.setDirection("up");
+	},
+	87: function(){
+		window.gameData.snake.setDirection("up");
+	},
+	39: function(){
+		window.gameData.snake.setDirection("right");
+	},
+	68: function(){
+		window.gameData.snake.setDirection("right");
+	},
+	40: function (){
+		window.gameData.snake.setDirection("down");
+	},
+	83: function (){
+		window.gameData.snake.setDirection("down");
+	},
+	80: function(){
+		window.game.pause();
+	},
+	77: function(){
+		window.game.initWithMode();
+	},
+	90: function(){
+		window.game.initWithMode(0);
+	},
+	88: function(){
+		window.game.initWithMode(1);
+	},
+	67: function(){
+		window.game.initWithMode(2);
+	},
+	82: function(){
+		window.game.restartGame();
+	}
+};
 
 window.addEventListener("keydown", function(e){
 	if(typeof keyEvents[e.keyCode] !== "undefined"){
